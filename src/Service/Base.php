f9eed4b76e717da2fff9e0bc968281c9dbca6eb7
Richard V <richard@richardvalvona.uk>
2021-07-27 22:55:03 +0100

    Update Base.php

--- src/Service/Base.php
+++ src/Service/Base.php
@@ -16,9 +16,9 @@ final class Base {
         session_start();
     
         $dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
-        $dbname = "my_website"; // the name of the database that you are going to use for this project
-        $dbuser = "root"; // the username that you created, or were given, to access your database
-        $dbpass = "pl0ske77"; // the password that you created, or were given, to access your database
+        $dbname = <Choose database name>; // the name of the database that you are going to use for this project
+        $dbuser = <Choose username>; // the username that you created, or were given, to access your database
+        $dbpass = <Choose password>; // the password that you created, or were given, to access your database
 
         $mysqli = mysqli_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysqli_error());
         $mysqli->select_db($dbname) or die("MySQL Error: " . mysqli_error());
